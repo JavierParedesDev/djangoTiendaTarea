db3b7cf4522ac8340eb9b05b53340b79a27d1b40
nAguayoValdebenito <n.aguayovaldebenito@gmail.com>
2024-07-09 01:07:54 -0400

    cambiosenpago3
    cambios en el boton y proceso pago

--- tienda/urls.py
+++ tienda/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('procesar-pago/', views.procesar_pago, name='procesar_pago'),
     path('login/', views.login_view, name='login'),
     path('editar-producto/<int:producto_id>/', views.editar_producto, name='editar_producto'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('procesar-pago/', views.procesar_pago, name='procesar_pago'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
